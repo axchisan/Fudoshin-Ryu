bb5447debce5d89d712e4bc757f0065cc87c14bc
axchisan <leftyrancuentabot@gmail.com>
2025-12-02 16:16:33 -0500

    errores en el theme provider

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -2,9 +2,5 @@
 import { ThemeProvider as NextThemesProvider, type ThemeProviderProps } from "next-themes"
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return (
-    <NextThemesProvider {...props} suppressHydrationWarning>
-      {children}
-    </NextThemesProvider>
-  )
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
